5d484ce545c8ae738592486a80830ba086255161
Chris Keenan <chriskeenan@gmail.com>
2020-08-05 14:14:01 -0400

    fix IP Address retrieval for requests. added other extensions to HttpRequests

--- Breaker.Helpers/Extensions/HttpRequestExtensions.cs
+++ Breaker.Helpers/Extensions/HttpRequestExtensions.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Primitives;
+using System;
+using System.Linq;
 
 namespace Breaker.Helpers.Extensions
 {
@@ -7,6 +10,53 @@ namespace Breaker.Helpers.Extensions
         public static string GetFullHostingUrlRoot(this HttpRequest request)
         {
             return $"{request.Scheme}://{request.Host.Value}";
+        }
+
+        public static string GetHeader(this HttpRequest request, string key)
+        {
+            return request.Headers.FirstOrDefault(x => x.Key == key).Value.FirstOrDefault();
+        }
+
+        public static string GetIPAddress(this HttpRequest request)
+        {
+            var result = string.Empty;
+            try
+            {
+                //first try to get IP address from the forwarded header
+                if (request.Headers != null)
+                {
+                    //the X-Forwarded-For (XFF) HTTP header field is a de facto standard for identifying the originating IP address of a client
+                    //connecting to a web server through an HTTP proxy or load balancer
+                    var forwardedHttpHeaderKey = "X-FORWARDED-FOR";
+
+                    var forwardedHeader = request.Headers[forwardedHttpHeaderKey];
+                    if (!StringValues.IsNullOrEmpty(forwardedHeader))
+                        result = forwardedHeader.FirstOrDefault();
+                }
+
+                //if this header not exists try get connection remote IP address
+                if (string.IsNullOrEmpty(result) && request.HttpContext.Connection.RemoteIpAddress != null)
+                    result = request.HttpContext.Connection.RemoteIpAddress.ToString();
+            }
+            catch
+            {
+                return string.Empty;
+            }
+
+            //some of the validation
+            if (result != null && result.Equals("::1", StringComparison.InvariantCultureIgnoreCase))
+                result = "127.0.0.1";
+
+            //remove port
+            if (!string.IsNullOrEmpty(result))
+                result = result.Split(':').FirstOrDefault();
+
+            return result;
+        }
+
+        public static string GetUserAgent(this HttpRequest request)
+        {
+            return request.Headers["User-Agent"].ToString();
         }
     }
 }
