372a228de208476455c005ace9f0c2a86217db7e
Chris Keenan <chriskeenan@gmail.com>
2020-09-02 13:59:54 -0400

    fix enum helpers for retrieving Display attribute name or description

--- Breaker.Helpers/Extensions/EnumExtensions.cs
+++ Breaker.Helpers/Extensions/EnumExtensions.cs
@@ -1,34 +1,51 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.Reflection;
 
 namespace Breaker.Helpers.Extensions
 {
     public static class EnumExtensions
     {
         /// <summary>
-        /// Returns the [Display(Name="")] attribute of an enum
+        /// Returns the [Display(Name="")] attribute of an enum, otherwise falls back to the enum base value
         /// </summary>
         /// <param name="value"></param>
         /// <returns></returns>
-        public static string GetDisplayName(this Enum value)
+        public static string GetDisplayName(this Enum enu)
         {
-            if (value == null)
+            var attr = GetDisplayAttribute(enu);
+
+            if (attr != null)
+                return attr.Name ?? enu.ToString();
+
+            return enu.ToString();
+        }
+
+        /// <summary>
+        /// Returns the [Display(Description="")] attribute of an enum, otherwise falls back to the enum base value
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static string GetDescription(this Enum enu)
+        {
+            var attr = GetDisplayAttribute(enu);
+            if (attr != null)
+                return attr.Description ?? enu.ToString();
+
+            return enu.ToString();
+        }
+
+        private static DisplayAttribute GetDisplayAttribute(object value)
+        {
+            var type = value.GetType();
+            if (!type.IsEnum)
             {
-                throw new ArgumentNullException(nameof(value));
+                throw new ArgumentException(string.Format("Type {0} is not an enum", type));
             }
 
-            var enumType = value.GetType();
-            var enumValue = Enum.GetName(enumType, value);
-            var member = enumType.GetMember(enumValue)[0];
-
-            var attrs = member.GetCustomAttributes(typeof(DisplayAttribute), false);
-            var outString = ((DisplayAttribute)attrs[0]).Name;
-
-            if (((DisplayAttribute)attrs[0]).ResourceType != null)
-            {
-                outString = ((DisplayAttribute)attrs[0]).GetName();
-            }
-            return outString;
+            // Get the enum field.
+            var field = type.GetField(value.ToString());
+            return field == null ? null : field.GetCustomAttribute<DisplayAttribute>();
         }
     }
 }
